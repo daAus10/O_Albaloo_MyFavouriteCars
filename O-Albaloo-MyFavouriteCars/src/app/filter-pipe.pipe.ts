8243f8fe574f205fa3a60f735eb48098981bed81
daAus10 <w0764215@myscc.ca>
2022-02-14 15:21:52 -0500

    assignment3 complete

--- O-Albaloo-MyFavouriteCars/src/app/filter-pipe.pipe.ts
+++ O-Albaloo-MyFavouriteCars/src/app/filter-pipe.pipe.ts
@@ -7,18 +7,15 @@ import { Car } from './helper-files/Car';
 export class FilterPipePipe implements PipeTransform {
 
 // filters input by the make of the car aka, ford, chevy , Tesla etc..
-  transform(tags: Car[]): Car[]{
+  transform(carList: Car[], make?: string){
     
-  
-  return tags.filter(function (tag){
-    return tag.tags != null ? tag.tags.length != 0 : null;
-    //  return showCars.filter(showCars => showCars.maker == maker ? showCars : null);
+      return carList.filter(showCars => showCars.maker == make ? showCars : null);
 
-});
+};
 
 
   }
   
-  }
+  
 
 
